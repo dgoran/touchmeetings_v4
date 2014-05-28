fdbeeeca5fc0f08dbcf41d6783e7934adf7b219a
dgoran <gdraskovic@gmail.com>
2014-05-28 11:27:43 -0400

    Update #3

---
+++ mobileapp/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,20 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button5 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button5.click = function button5_click (event)// @startlock
+	{// @endlock
+		// Save edits
+		sources.speaker.save
+		
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button5", "click", button5.click, "WAF");
+// @endregion
+};// @endlock
