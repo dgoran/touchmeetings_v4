dc4447c2cdb369657e482e6e92a87bf4fed65231
dgoran <gdraskovic@gmail.com>
2014-06-02 10:49:55 -0400

    Update #24

--- mobileapp/Model/Venue/Venue-events.js
+++ mobileapp/Model/Venue/Venue-events.js
@@ -3,7 +3,7 @@
 model.Venue.fullAddress.onGet = function() {
 	// Add your code here;
 	
-		return this.street + ','+' ' + this.city + ' ' + this.state + ' ' + this.zip;
+//		return this.street + ','+' ' + this.city + ' ' + this.state + ' ' + this.zip;
 
 	
 };
